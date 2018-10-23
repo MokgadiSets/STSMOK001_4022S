675d20859a64ea09624819533c588f4d22326541
MokgadiSets <44416265+MokgadiSets@users.noreply.github.com>
2018-10-24 00:10:18 +0200

    Update Monocular.m

--- Code/Monocular.m
+++ Code/Monocular.m
@@ -1,39 +1,42 @@
+% This is an object detector using one camera and an image stored in your file system
+%For real time detector, see webcam.m
+
 clear all;
 close all;
-a = imread('ocean1.jpg');
+a = imread('ocean1.jpg'); % put in any image from file
 
-detector = cv.FeatureDetector('KAZE');%,'MaxFeatures',75);
-%detector = cv.FeatureDetector('FastFeatureDetector');%,'MaxFeatures',75);
- tic
-keypoints1 = detector.detect(a); 
-keypoints = cv.KeyPointsFilter.retainBest(keypoints1, 75);
- toc
-points= [];
-imshow(cv.putText(a,'',[50,50],'BottomLeftOrigin', false, 'FontScale',0.5, 'Color','r'));
+
+detector = cv.FeatureDetector('FastFeatureDetector');%,'MaxFeatures',75); %could use any other feature detector.
+
+keypoints1 = detector.detect(a); %detect keypoints
+keypoints = cv.KeyPointsFilter.retainBest(keypoints1, 400); %retain the best 400
+ 
+points= [];   %array for storing pointd
+imshow(a);    %plot image
 hold on;
-for z =1: length(keypoints)
+for z =1: length(keypoints)         %This loop is for plotting keypoints.
         %points = [points; keypoints(z).pt];
-        plot(keypoints(z).pt(1),keypoints(z).pt(2) ,'x', 'LineWidth', 1, 'Color', 'red')
+        plot(keypoints(z).pt(1),keypoints(z).pt(2) ,'x', 'LineWidth', 1, 'Color', 'red') 
 end
 
-%[labels, centers, compactness] = cv.kmeans(points,  2, 'Attempts', 4);
-[C, ptsC, centres] = dbscan(transpose(points), 30, 10);
-% for d = 1: max(ptsC)
-%     ToPlot = [];
-%     for c = length(keypoints):-1:1
-%         if (ptsC(c)<1)%(keypoints(c).response < 3500)||(ptsC(c)<1)
-%             keypoints(c) = [];
-%             ptsC(c) = [];
-%   
-%         elseif isequal(ptsC(c),d)
-%             ToPlot = [ToPlot; keypoints(c).pt];
-%         end
-%     end
-%     output = cv.boundingRect(ToPlot);
-%     hold on; rectangle('Position', output,'EdgeColor','r', 'LineWidth',1);
-% end
 
-%
+[C, ptsC, centres] = dbscan(transpose(points), 30, 10); %form clusters
+for d = 1: max(ptsC)
+    ToPlot = [];
+    for c = length(keypoints):-1:1   %This loop is for plotting bounding boxes
+        if (ptsC(c)<1)    %remove points classified as noise
+            keypoints(c) = [];
+            ptsC(c) = [];
+  
+        elseif isequal(ptsC(c),d)
+            ToPlot = [ToPlot; keypoints(c).pt];
+        end
+    end
+   output = cv.boundingRect(ToPlot);
+   hold on; rectangle('Position', output,'EdgeColor','r', 'LineWidth',1); % Plot each bounding rect
+end
+
+
 hold off;
-%toc
+
  
